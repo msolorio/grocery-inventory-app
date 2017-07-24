b2f333d177a8bba0d9e7c2a20daafdddb605b9ad
msolorio <msolorio789@gmail.com>
2017-07-23 18:14:54 -0700

    [wip] adding route structure

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,85 @@
+// 'use strict';
+
+const path = require('path');
+
 module.exports = function(app, passport) {
 
-	require('./login')(app, passport);
+	// HOMEPAGE ////////////////////////////////////////////////
+	app.get('/', isLoggedIn, (req, res) => {
+
+		res.sendFile(path.join(__dirname + '/../../views/index.html'));
+	});
+
+	// LOGIN PAGE //////////////////////////////////////////////
+	app.get('/login', isLoggedIn, (req, res) => {
+		res.render('login.ejs', { message: req.flash('loginMessage')});
+	});
+
+	// LOGIN ATTEMPT (custom authentication handling) //////////
+	app.post('/login', (req, res, next) => {
+
+		passport.authenticate('local-login', (err, user, info) => {
+			if (err) return next(err);
+
+			if (!user) return res.redirect('/login');
+			
+			// req.login establishes a session
+			req.login(user, (loginErr) => {
+				if (loginErr) return next(loginErr);
+
+				res.redirect(`/profile/${user.local.username}`);
+			});
+		})(req, res, next);
+
+	});
+
+	// SINGUP PAGE /////////////////////////////////////////////
+	app.get('/signup', isLoggedIn, (req, res) => {
+		res.render('signup.ejs', { message: req.flash('signupMessage')});
+	});
+
+	// SIGNUP ATTEMPT (custom authentication handling) /////////
+	app.post('/signup', (req, res, next) => {
+
+		passport.authenticate('local-signup', (err, user, info) => {
+			if (err) return next(err);
+
+			if (!user) return res.redirect('/signup');
+
+			req.login(user, (loginErr) => {
+				if (loginErr) return next(loginErr);
+
+				res.redirect(`/profile/${user.local.username}`);
+			});
+		})(req, res, next);
+
+	});
+
+	// PROFILE PAGE /////////////////////////////////////////////
+	// checks url params for when route is hit directly
+	app.get('/profile/:username', (req, res) => {
+
+		if (req.user &&
+			 (req.user.local.username === req.params.username &&
+		 		req.isAuthenticated())) {
+			return res.sendFile(path.join(__dirname + '/../../views/profile.html'));	
+		}
+
+		res.redirect('/');
+	});
+
+	// LOGOUT ///////////////////////////////////////////////////
+	app.get('/logout', (req, res) => {
+		req.logout();
+		res.redirect('/');
+	});
 
 }
+
+function isLoggedIn(req, res, next) {
+	if (req.user && req.isAuthenticated()) {
+		return res.redirect(`/profile/${req.user.local.username}`);	
+	}
+	next();
+}
+
