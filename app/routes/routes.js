55a1b6ac7f2b48a29d724bc4b22eb704cbbe935b
msolorio <msolorio789@gmail.com>
2017-07-23 17:50:52 -0700

    handle redirecting logged in users

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,18 +6,15 @@ const path = require('path');
 module.exports = function(app, passport) {
 
 	// HOMEPAGE ////////////////////////////////////////////////
-	app.get('/', (req, res) => {
-		// res.send(__dirname + '/../../views/index.html')
+	app.get('/', isLoggedIn, (req, res) => {
+
 		res.sendFile(path.join(__dirname + '/../../views/index.html'));
 	});
 
 	// LOGIN PAGE //////////////////////////////////////////////
-	app.get('/login', (req, res) => {
+	app.get('/login', isLoggedIn, (req, res) => {
 		res.render('login.ejs', { message: req.flash('loginMessage')});
 	});
-
-	// example of custom handling of authentication
-	// https://stackoverflow.com/questions/15711127/express-passport-node-js-error-handling
 
 	// LOGIN ATTEMPT (custom authentication handling) //////////
 	app.post('/login', (req, res, next) => {
@@ -37,14 +34,8 @@ module.exports = function(app, passport) {
 
 	});
 
-	// app.post('/login', passport.authenticate('local-login', {
-	// 	successRedirect: '/profile',
-	// 	failureRedirect: '/login',
-	// 	failureFlash: true
-	// }));
-
 	// SINGUP PAGE /////////////////////////////////////////////
-	app.get('/signup', (req, res) => {
+	app.get('/signup', isLoggedIn, (req, res) => {
 		res.render('signup.ejs', { message: req.flash('signupMessage')});
 	});
 
@@ -65,14 +56,16 @@ module.exports = function(app, passport) {
 
 	});
 
-	// app.post('/signup', passport.authenticate('local-signup', {
-	// 	successRedirect: '/profile',
-	// 	failureRedirect: '/signup',
-	// 	failureFlash: true
-	// }));
+	// checks url params for when route is hit directly
+	app.get('/profile/:username', (req, res) => {
 
-	app.get('/profile/:username', isLoggedIn, (req, res) => {	
-		res.sendFile(path.join(__dirname + '/../../views/profile.html'));
+		if (req.user &&
+			 (req.user.local.username === req.params.username &&
+		 		req.isAuthenticated())) {
+			return res.sendFile(path.join(__dirname + '/../../views/profile.html'));	
+		}
+
+		return res.redirect('/');
 	});
 
 	// LOGOUT ///////////////////////////////////////////////////
@@ -84,17 +77,9 @@ module.exports = function(app, passport) {
 }
 
 function isLoggedIn(req, res, next) {
-	const cookiedUser = req.user && req.user.local.username || null;
-	const paramsUser = req.params.username || null;
-	if (cookiedUser &&
-			paramsUser &&
-			paramsUser === cookiedUser &&
-			req.isAuthenticated()) {
-		console.log('req.isAuthenticated is true');
-		return next();
+	if (req.user && req.isAuthenticated()) {
+		return res.redirect(`/profile/${req.user.local.username}`);	
 	}
-
-	console.log('req.isAuthenticated is false');
-	res.redirect('/');
+	next();
 }
 
